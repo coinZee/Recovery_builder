2f57d8219af0bd279c3876bd1c7d37773ca73fac
coinZee <coinz1112@gmail.com>
2023-10-07 07:59:39 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ export BRANCH="twrp-12.1" # the branch of manifest
 
 # about your device
 export DEVICE="X6835" # codename used in device tree
-export DT_LINK="git@github.com:coinZee/Infinix-X6835.git" # device tree link
+export DT_LINK="https://github.com/coinZee/Infinix-X6835.git" # device tree link
 export DT_BRANCH="main" # device tree branch
 export VENDOR="infinix" # device manufacturer or vendor
 export TARGET="bootimg" # build method: recoveryimage, bootimage, vendorboot
