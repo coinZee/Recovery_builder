c5c2ba1f8bfe6b6f4f19582f0d229f84a7217eca
coinZee <coinz1112@gmail.com>
2023-10-07 05:50:52 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,15 +3,15 @@
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
 export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export BRANCH="twrp-12.1" # the branch of manifest
 
 # about your device
-export DEVICE="X6835B" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DEVICE="X6835" # codename used in device tree
+export DT_LINK="git@github.com:coinZee/Infinix-X6835.git" # device tree link
 export DT_BRANCH="twrp" # device tree branch
 export VENDOR="infinix" # device manufacturer or vendor
-export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export TARGET="bootimg" # build method: recoveryimage, bootimage, vendorboot
+export TYPE="twrp" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
