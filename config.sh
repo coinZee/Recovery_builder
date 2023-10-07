da0c410bccabc03a4c47768594643b5d7d8b8e82
coinZee <coinz1112@gmail.com>
2023-10-07 09:49:43 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,8 +2,8 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git" # the link of manifest
+export BRANCH="twrp-12.1" # the branch of manifest
 
 # about your device
 export DEVICE="Infinix-X6835" # codename used in device tree
@@ -11,7 +11,7 @@ export DT_LINK="https://github.com/coinZee/Infinix-X6835.git" # device tree link
 export DT_BRANCH="main" # device tree branch
 export VENDOR="infinix" # device manufacturer or vendor
 export TARGET="bootimg" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export TYPE="twrp" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
